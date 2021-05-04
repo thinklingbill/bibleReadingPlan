aae48516d9f24566e70b14edb6da619addec5dfe
Bill Roberts <bill@billjill.org>
2021-05-03 21:39:27 -0500

    Added daily words read calculations and error checks against the plan

--- dailyWordCount.php
+++ dailyWordCount.php
@@ -4,10 +4,6 @@
 # also validates that each chapter and verse is read once
 # note: kjv jsons in the Bible-kjv directory come from https://github.com/aruljohn/Bible-kjv.git
 
-echo "Read in the current bible reading plan Json exported from mysql workbench\n";
-$plan =  json_decode( file_get_contents(  "bibleReadingPlan.json" ) );
-#print_r( $plan );
-
 echo "Read in list of kjv books\n";
 $kjvBook =  json_decode( file_get_contents(  "Bible-kjv/Books.json" ) );
 #print_r( $kjvBook );
@@ -16,6 +12,7 @@ echo "Check for book name/book id mismatches\n";
 # (this is a brute-force method of getting
 # a unique list of book names from the plan)
 $planBook = array();
+$plan =  json_decode( file_get_contents(  "bibleReadingPlan.json" ) );
 
 foreach( $plan as $p ) {
     $planBook[ $p->{ "book_id" } ] = $p->{"book_name"};
@@ -42,10 +39,10 @@ print "$errors Errors detected\n";
 
 # if no errors, continue
 $counts = array();
+$read = array();
 if ( !$errors ) {
     # gather the chapters, verses and number of words per verse in the bible
     foreach ( $kjvBook as $b ) {
-        $b = "1 John";
         $jsonFile = "Bible-kjv/" . str_replace( " ", "", $b ) . ".json";
         print( "Processing $jsonFile\n");
         $j =  json_decode( file_get_contents(  $jsonFile ) );
@@ -53,27 +50,86 @@ if ( !$errors ) {
         # read through the json, gathering chapters, verses per chapter, and word counts per chapter and verse
         $chapterCount = 0;
         foreach( $j->{"chapters"} as $c ) {
-            $chapterCount++;            
-            print( "Chapter: " . $c->{"chapter"} . "\n" );
+            $chapterCount++;  
+            $chap = $c->{"chapter"};          
+#            print( "Chapter: " . $c->{"chapter"} . "\n" );
             $verseCount = 0;
             $wordsPerChapter = 0;
             foreach( $c->{"verses"}  as $v ) {
                 $wordsPerVerse = str_word_count( $v->{"text"}, 0 );
-                print( "Verse: " . $v->{"verse"} . ", words: $wordsPerVerse\n" );
+#                print( "Verse: " . $v->{"verse"} . ", words: $wordsPerVerse\n" );
                 #print( "Text:" . $v->{"text"} . "| Word count: $wordsPerVerse\n" );
+                $verse = $v->{"verse"};
                 $wordsPerChapter += $wordsPerVerse;
                 $verseCount++;
+                $counts[ $b ][ "$chap.$verse"] = $wordsPerVerse;
+                $read[ $b ][ "$chap.$verse" ] = 0;
             }
-            print( "Words for chapter " . $c->{"chapter"} . ": $wordsPerChapter\n");
+#            print( "Words for chapter " . $c->{"chapter"} . ": $wordsPerChapter\n");
+#            print_r( $counts ); die;
         }
-        die;
     }
-
-
 }
 
+echo "Read in the current bible reading plan Json exported from mysql workbench\n";
 
-#print_r ( $planBook );
+$dayWords = array();
+$count = 0;
+foreach ( $plan as $p ) {
+    $count++;
+    # this is exceptionally brute force...
+    $bookName = $p->{"book_name"};
+    foreach( $counts[ $bookName ] as $key => $value) {
+        $k = explode( ".", $key );
+        # full chapter
+        if ( $p->{"start_verse"} == 0 ) {
+            if ( $k[0] >= $p->{"start_chapter"} and $k[0] <= $p->{"end_chapter"}) {
+#               print( "marking $bookName, $key to 1\n" );
+                $read[$bookName][$key]++;
+                $dayWords[ $p->{"day_id"} ] += $value;
+            }
+        }
+        else { # partial chapter. NOTE: assume no cros-chapter reading
+            if ( $k[0] == $p->{"start_chapter"} and $k[1] >= $p->{"start_verse"} and $k[1] <= $p->{"end_verse"}) {
+                $read[$bookName][$key]++;
+                $dayWords[ $p->{"day_id"} ] += $value;                
+             }
+        }
+    }
+
+#    if ( $count > 5 ) die;
+}
+
+# find any verses read more than once or not at all
+$dupReads = 0;
+$problem = 0;
+foreach ( $read as $k => $ar ) {
+    foreach( $ar as $j => $value ) {
+        if ( $value == 0 ) { 
+            print( "$k, chapter.verse $j is never read\n");
+            $problem++;
+        }
+        if ( $value > 1 ) { 
+            print( "$k, chapter.verse $j is read $value times\n");
+            $dupReads += $counts[ $k ][ $j ];
+            $problem++;
+        }
+    }
+}
+if ( $dupReads > 0 ) {
+    print( "Words duplicated: $dupReads\n");
+}
+
+#if ( $problem ) die;
+
+$total = 0;
+foreach( $dayWords as $key => $value ) {
+    print( "$key = $value\n");
+    $total += $value;
+}
+
+print ( "TOTAL WORDS: $total\n");
+
  
 #    if ( !array_key_exists( $dayId, $versesPerDay ) ) {
 
