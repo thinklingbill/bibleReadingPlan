14c1a6b4df6c35ad8d09c96b18089ec2aeb2ea4e
Bill Roberts <bill@billjill.org>
2022-04-23 21:30:55 -0500

    Removed files associated with legacy perl/php version, added new plan builder, and refactored scala object names

---
+++ src/main/java/bibleTextIniter.scala
@@ -0,0 +1,52 @@
+import org.apache.spark.sql.SparkSession
+import functions._
+
+// initializes/loads the bible book table
+object initBibleText {
+  def loadBibleText(spark: SparkSession
+                    , schema: String
+                    , table: String
+                    , inFile: String): Unit = {
+
+    val processName = "loadBibleText"
+
+    logIt( s"$processName: drop and recreate $schema.$table")
+    spark.sql(s"drop table if exists $schema.$table")
+
+    spark.sql(
+      s"""
+         |create table $schema.$table (
+         |   book_abbr       string
+         |  ,chapter         integer
+         |  ,verse           integer
+         |  ,verse_text      string
+         |  ,word_count      integer
+         |)
+         |stored as ORC
+         |""".stripMargin
+    )
+
+    logIt( s"$processName: read in seed data from $inFile")
+    spark.read.option("delimiter", "|").csv(inFile)
+        .createOrReplaceTempView("t")
+
+    logIt( s"$processName: calculate word count and assign column names")
+    // notice collapsing multiple spaces down to one
+    val df = spark.sql(
+      s"""
+         |select _c0 as book_abbr
+         |     , cast( _c1 as integer ) as chapter
+         |     , cast( _c2 as integer ) as verse
+         |     , trim( _c3 ) as verse_text
+         |     , size( split( trim( regexp_replace( _c3, ' +', ' ' ) ), ' ' ) ) as word_count
+         |  from t
+         |""".stripMargin
+    )
+
+    logIt( s"$processName: insert data into $schema.$table")
+    df
+      .write
+      .mode( "append")
+      .insertInto( s"$schema.$table")
+  }
+}
